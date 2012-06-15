5f096d377902b74631269e22be4c2648cdcfafdf
Bob Miller Book Room <bmbr@OrderDesk.local>
2012-06-15 13:36:33 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='bookdb',
+      version='0.0',
+      description='bookdb',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='bookdb',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = bookdb:main
+      [console_scripts]
+      initialize_bookdb_db = bookdb.scripts.initializedb:main
+      """,
+      )
+
