6b4060dd5d88b8ed14dbf197b8fead9ce99c7afd
Simon Malcolm <simon.malcolm@gmail.com>
2012-07-05 16:11:38 -0400

    module to feed dummy info to the printing moduel for testing

---
+++ bookdb/tests/print_tests.py
@@ -0,0 +1,43 @@
+from bookdb.printing import generate_order_pdf
+from bookdb.models import Book, Distributor, Order, OrderEntry, ShippingMethod, Binding, Publisher, ShelfLocation
+
+from dateutil.parser import parse as parse_date
+
+_default_filename = 'test.pdf'
+
+
+def make_test_pdf(filename=_default_filename):
+    dummy_order = Order(
+        '1A1000',
+        parse_date('2012-1-1'),
+        Distributor(
+            'Warehouse Co.',
+            account_number='42',
+            fax='555-5543',
+            phone='416-555-9876',
+            sales_rep='Billy Jo',
+            address1='123 Fake Street',
+            city="Townville",
+            province="Mare Crisium",
+            postal_code="ABC123",
+            country="The Moon"),
+        ShippingMethod("Rocket"),
+        'Extra nonsense is free of charge!')
+    paperback = Binding('Paper')
+    penguin = Publisher('Penguin')
+    location = ShelfLocation('Dreamspace')
+    for x in xrange(10, 35):
+            OrderEntry(
+            dummy_order,
+            Book(
+                '97811122233' + str(x),
+                'BOOK' + str(x),
+                penguin,
+                paperback,
+                location,
+                [('Seneca',), ('Brown', 'Dan')]),
+            x)
+    generate_order_pdf(dummy_order, filename)
+
+if __name__ == '__main__':
+    make_test_pdf()
